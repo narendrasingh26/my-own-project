15e1701040371be976ea7cadf03d845596488710
narendrasingh26 <narendrajadon261990@gmail.com>
2023-02-25 18:35:12 +0530

    my own project

---
+++ dom.js
@@ -0,0 +1,21 @@
+// const parent =document.getElementsByTagName('body');
+// heading.innerHTML='ALOK_IS_GREA'
+// const parent=heading.parentNode;
+// console.log(parent);
+
+// next elementby sibling//
+
+// const heading=document.querySelector('h1');
+// console.log(heading.nextElementSibling);
+
+
+// privious element sibling//
+
+
+
+// const heading=document.querySelector('h1');
+// console.log(heading.previousElementSibling);
+
+// manuplation//
+// const heading=document.querySelector('h1');
+// heading.innerHTML='the_best_form'
